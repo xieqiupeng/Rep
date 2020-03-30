c409f5baab359549fe8e9b57177a2420340ec78c
xieqiupeng <xieqiupeng@gmail.com>
2020-03-30 11:54:11 +0800

    udpate

---
+++ UnSatisfy/dependency.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+# Dependency
+ln -sf /Volumes/Project/Classify/Trust  $Path/Trust
+ln -sf /Volumes/Project/Classify/Signal  $Path/Signal
+ln -sf /Volumes/Project/Enum/Enum-Lesson/信号  $Path/信号
+
+# Root
+Path=/Volumes/Project/Classify/UnChange
+
+Path=/Volumes/Project/Classify
+ln -sf /Volumes/Project/Enum/Enum-Coder/Technology  $Path/Technology
