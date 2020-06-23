f311528e6205cab0230e25b577ece94d3934d39d
xieqiupeng <xieqiupeng@gmail.com>
2020-06-24 05:40:41 +0800

    update

---
+++ Play/dependency.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+# Dependency
+ln -sf /Volumes/Project/Enum/Enum-Lesson/好关系  $Path/第1季/强力瞬间/4/好关系
+ln -sf /Volumes/Project/Classify/Distance  $Path/Distance
+
+
+# Run
+Path=/Volumes/Project/Classify/Place
+
+
+
+
